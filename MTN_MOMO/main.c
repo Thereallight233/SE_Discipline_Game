8ef9aca80710dcdff187a8a0e083ee8e5a208169
Jonathan Lartey <larteycorp14@gmail.com>
2023-12-06 21:26:21 +0000

    displayMainMenu Function Implementation

---
+++ MTN_MOMO/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+void displayMainMenu(void)
+{
+    // Print the main menu options
+    printf("Main Menu:\n");
+    printf("a. Transfer Money\n");
+    printf("b. Airtime and Bundles\n");
+    printf("c. Allow Cashout\n");
+    printf("d. Exit\n");
+}
