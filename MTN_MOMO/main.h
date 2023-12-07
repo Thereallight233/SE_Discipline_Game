f6f5ad4c104352942a47e4c22ca56790f0146f93
Jonathan Lartey <larteycorp14@gmail.com>
2023-12-07 02:36:06 +0000

    contains function prototypes

--- MTN_MOMO/main.h
+++ MTN_MOMO/main.h
@@ -2,6 +2,12 @@
 #define MAIN_H
 
 // Function prototypes
-void displayMainMenu(void);
-
+void displayMainMenu();
+void transferMoney();
+void airtimeAndBundles();
+void allowCashout();
+void displayTransferOptions();
+void displayAirtimeBundlesOptions();
+void mobileMoneyProgram();
 #endif
+
