3d5407e7fbfa1fa29cf55d7cf3f1cde0e4ec3a6c
Jonathan Lartey <larteycorp14@gmail.com>
2023-12-07 02:36:52 +0000

    MTN_MoMo.c

--- MTN_MOMO/MTN_MoMo.c
+++ MTN_MOMO/MTN_MoMo.c
@@ -1,20 +1,9 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
-    // Show a welcome message
-    printf("Welcome to the Mobile Money Program!\n");
+	mobileMoneyProgram();
 
-    // Wait for the user to type in a special code (USSD code)
-    char ussdCode[10];
-    printf("Enter USSD code: ");
-    scanf("%s", ussdCode);
-
-    if (strcmp(ussdCode, "*170#") == 0)
-        // If the code is "*170#", go to the main menu
-        displayMainMenu();
-
-return (0);
+	return (0);
 }
